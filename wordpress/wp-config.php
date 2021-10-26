2b1c620be102a31091a3bb58c1abbe17e2eb3e08
Jonathan-Caillon <j.caillon@codeur.online>
2021-10-26 10:56:51 +0200

    config db

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -22,13 +22,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'foodbox' );
+define( 'DB_NAME', 'u233458275_wordpress' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u233458275_nora' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Nora01500' );
 
 /** Adresse de l’hébergement MySQL. */
 define( 'DB_HOST', 'localhost' );
